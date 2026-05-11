2cb6ac6a47472f49521cf15a23c94a9da82d713e
tulasivaindamfed9 <tulasivaindamfed9@gmail.com>
2026-05-11 12:20:50 +0530

    fe live url

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["*"],
-     allow_origins=["https://your-frontend.vercel.app"],
+     allow_origins=["https://airesumemockinterviewfe.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
